afc27cda6162134d65055291b2010838dfdcdc7b
nolan-2026 <nrnelsen@students.unwsp.edu>
2026-02-20 14:04:56 -0600

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -1,3 +1,7 @@
+# By Nolan Nelsen
+# Written on 2/20/2026
+# Math Quiz
+
 # Program #2: Math Quiz
 # Write a program that gives simple math quizzes.  The program should display two random numbers to be added, such as
 
@@ -7,7 +11,28 @@
 
 # ------
 
-# The program should allow the student to enter the answer.  
-# If the answer is correct, a message of congratulations should be displayed.  
-# If the answer is incorrect a message showing the correct answer should be displayed.  
+import random
+
+def math_equation(answer):
+    answer = 0.0
+
+    number1 = random.randint(1, 999)
+    number2 = random.randint(1, 999)
+
+    print(f"What is {number1} + {number2}?")
+    answer = float(input("Your answer: "))
+
+    actual_answer = number1 + number2
+
+    if answer == actual_answer:
+        print("Congratulations, your answer is correct!")
+    else:
+        print(f"Your answer is incorrect. The answer is {actual_answer}.")
+
+math_equation(0)
+
+
+# The program should allow the student to enter the answer.
+# If the answer is correct, a message of congratulations should be displayed.
+# If the answer is incorrect a message showing the correct answer should be displayed.
 # The program must use a function that accomplishes part of the needed tasks.
